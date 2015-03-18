9ead97cf1d8f803df5673c95e28a8f36b760b3a6
swhiteley91 <simonwhiteley@hotmail.com>
2015-03-18 22:51:19 +0100

    initial commit

---
+++ flightweather/src/main/java/nl/gsos/jelle/simon/flightweather/App.java
@@ -0,0 +1,13 @@
+package nl.gsos.jelle.simon.flightweather;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
