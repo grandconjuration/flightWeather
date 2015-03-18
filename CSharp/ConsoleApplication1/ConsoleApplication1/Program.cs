072ed83974afc388848d575bc58350366af12101
Jelle Luteijn <jelle.luteijn@gmail.com>
2015-03-18 23:21:21 +0100

    C# projectje om de soap client te kunnen weten

---
+++ CSharp/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApplication1.GlobalWeather;
+using ConsoleApplication1.RouteSoapApiService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalWeatherSoapClient weatherSoapClient = new GlobalWeatherSoapClient();
+            //weatherSoapClient.
+
+            routeServiceClient flightSoapClient = new routeServiceClient();
+            //flightSoapClient.
+        }
+    }
+}
